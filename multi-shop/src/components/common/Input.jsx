e9fdad4c057c8b226d4fe00fcb7857f4d425acf3
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-08-10 10:41:43 +0530

    update input component

--- multi-shop/src/components/common/Input.jsx
+++ multi-shop/src/components/common/Input.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-function Input() {
-  return (
-    <div>Input</div>
-  )
-}
+const Input = ({ label, type = 'text', name, value, onChange, placeholder = '', required = false }) => {
+    return (
+        <div className="input-field">
+            {label && <label htmlFor={name}>{label}</label>}
+            <input
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                required={required}
+                id={name}
+                className="input"
+            />
+        </div>
+    );
+};
 
-export default Input
+export default Input;
